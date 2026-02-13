725b8e6ab05ca1c2efe3a841f17c17ee7733d833
Arseniy <natali.astafjewa2017@yandex.ru>
2026-02-13 19:07:23 +0300

    ma ? commit

---
+++ api/main.py
@@ -0,0 +1,11 @@
+from flask import Flask, render_template, url_for
+from flask_sqlalchemy import SQLAlchemy
+from flask_login import LoginManager, UserMixin
+
+app = Flask(__name__, template_folder='../templates', static_folder="../static")
+
+app.config["SQLALCHEMY_DATABASE_URI"] = 'sqlite:///nexus.db'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+db = SQLAlchemy(app)
+lm = LoginManager(app)
